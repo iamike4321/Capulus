35e165b42dd98c150dc63bff426f9096dd938a60
Vincent Tran <vincent.tran499@524-12h4g8wn.local>
2017-05-17 17:25:53 -0700

    finished everything up,  if you want changes just hmu about it

--- js/functions.js
+++ js/functions.js
@@ -8,22 +8,31 @@ function removeImage() {
 
 // wait time for chat, contact page
 function waitTime() {
-  var time = [15,10,3,11,20,13,18];
-  var x = Math.floor((Math.random() * 7) + 1);
-
-  alert("Your Approximate Wait time is "+time[x]+" minute(s).");
+  //array that picks a number
+  var time = [7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  //randomizer to pick a number from 1-7
+  var x = Math.floor((Math.random() * 14) + 1);
+//pops an alert on how long you will have to wait in chat
+  alert("Your Approximate Wait time is "+time[x]+" minute(s)." +
+      "Please click okay to continue and wait...");
 }
 
-//form validation
+//form validation for contact page
 function validateForm() {
     var x = document.forms["form1"]["tel"].value;
-    // if fname is empty then there will be an alert
+    // checks if x or telephone value is a number or not.
     if (isNaN(x)) {
-        alert("Please fill out phone # with ONLY numbers");
+        alert("Please fill out a proper phone number with ONLY numbers");
         return false;
     }else {
-      alert("true")
       return true;
-
     }
   }
+
+  //menu page on setInterval command
+var increment = window.setInterval(counter, 1500);
+function counter(inc) {
+  var inc = document.getElementById('counter').value;
+  inc++;
+  document.getElementById('counter').value = inc;
+}
