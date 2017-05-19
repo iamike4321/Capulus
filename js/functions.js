ab2a8e5e7a141a38f64c151f320911711fffcbd9
Isaac Meder <isaac.meder485@west-mec.org>
2017-05-19 16:51:05 -0700

    Version 1.2 almost perfect

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,55 @@
-// showing and removing image in menu page
-function showImage() {
-  document.getElementById('ogImg').innerHTML = "<img src='images/cImg.jpeg'>";
-}
-function removeImage() {
-    document.getElementById('ogImg').innerHTML =" ";
-}
+// ===========================================
+//                 CONTACT PAGE
+// ===========================================
 
 // wait time for chat, contact page
 function waitTime() {
-  var time = [15,10,3,11,20,13,18];
-  var x = Math.floor((Math.random() * 7) + 1);
-
-  alert("Your Approximate Wait time is "+time[x]+" minute(s).");
+  //array that holds integers of random amounts
+  var time = [13, 16, 12, 20, 31, 37, 28, 10, 5, 1, 39, 14, 22, 59];
+  //randomizer to pick a number from 1-14, rounds a random math number and multiples it by 14, adds 1, (14 is the number same to the array's length.)
+  var x = Math.floor((Math.random() * 14) + 1);
+  //pops an alert on how long you will have to wait in chat
+  alert("Your Approximate Wait time is " + time[x] + " minute(s)." +
+    "Please click okay to continue and wait...");
 }
 
-//form validation
+//form validation for contact page
 function validateForm() {
-    var x = document.forms["form1"]["tel"].value;
-    // if fname is empty then there will be an alert
-    if (isNaN(x)) {
-        alert("Please fill out phone # with ONLY numbers");
-        return false;
-    }else {
-      alert("true")
-      return true;
-
-    }
+  //var x is the value taken from a form named form1,with an id of tel.
+  var x = document.forms["form1"]["tel"].value;
+  // checks if x or telephone value is a number or not.
+  if (isNaN(x)) {
+    alert("Please fill out a proper phone number with ONLY numbers");
+    return false;
+  } else {
+    return true;
   }
+}
+
+// ===========================================
+//                 MENU PAGE
+// ===========================================
+
+// showing and removing image in menu page
+function showImage() {
+  //sets the span tag's innerHTML to an image html tag with src filled in.
+  document.getElementById('ogImg').innerHTML = "<img src='images/cImg.jpeg'>";
+}
+
+function removeImage() {
+  //removes span tags innerHTML so the image disappears.
+  document.getElementById('ogImg').innerHTML = " ";
+}
+
+//menu page on setInterval command
+var increment = window.setInterval(counter, 1500);
+
+function counter(inc) {
+  //set var inc to the value of the counter,
+  // adds 1 to it, then sets that value to inc to repeat
+  var inc = document.getElementById('counter').value;
+  inc++;
+  document.getElementById('counter').value = inc;
+}
+
+//p.s. I love you Clawson - The Lead Developer. <3
